5d05f3060d0f358f63519b0b70defce38cef9a33
Jonathan Garcia Speçamillio <jgspecamillio@gmail.com>
2023-04-17 21:38:55 -0300

    profile button add

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -21,39 +21,75 @@ class _HomePageState extends State<HomePage> {
           ),
           Positioned(
             top: 70,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.5),
-                      spreadRadius: 8,
-                      blurRadius: 10,
-                      offset: Offset(0, 2), // changes position of shadow
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    spreadRadius: 8,
+                    blurRadius: 10,
+                    offset: Offset(0, 2), // changes position of shadow
+                  ),
+                ], color: Color.fromARGB(255, 240, 240, 240)),
+                child: Column(
+                  children: [
+                    Container(
+                      color: Color.fromARGB(255, 240, 240, 240),
+                      height: 10,
+                      width: MediaQuery.of(context).size.width,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Color.fromARGB(255, 233, 233, 233),
+                      ),
+                      height: 60,
+                      width: MediaQuery.of(context).size.width - 20,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: Row(
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.only(left: 10),
+                                  child: CircleAvatar(
+                                    backgroundColor: Colors.amber,
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.only(top: 10),
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.only(left: 10),
+                                        child: Text(
+                                          'Bom dia!',
+                                          style: TextStyle(fontSize: 12),
+                                        ),
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.only(left: 10),
+                                        child: Text('Matheus Soldera'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(right: 10),
+                            child: Text('Ver meu perfil'),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15)),
-                  color: Color.fromARGB(255, 240, 240, 240)),
-              child: Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () => Navigator.of(context).pop(),
-                          child: Icon(
-                            Icons.arrow_back_outlined,
-                            color: Color.fromRGBO(0, 9, 89, 1),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
           ),
