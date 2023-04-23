e1662bc94905acc6b71e4c1e66f56dc308ea3b7b
Pedro Soldera <pedro-soldera2011@hotmail.com>
2023-04-22 17:19:45 -0700

    Adicionando função de rolagem horizontal

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -52,112 +52,119 @@ class _HomePageState extends State<HomePage> {
                   offset: Offset(0, 2), // changes position of shadow
                 ),
               ], color: Color.fromARGB(255, 240, 240, 240)),
-              child: GestureDetector(
-                onTap: () => Get.to(UserProfilePage(),
-                    transition: Transition.noTransition),
-                child: Container(
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(top: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          color: Color.fromARGB(255, 233, 233, 233),
-                        ),
-                        height: 60,
-                        width: MediaQuery.of(context).size.width - 20,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              padding: EdgeInsets.only(left: 10),
-                              child: Row(
-                                children: [
-                                  Container(
-                                    child: CircleAvatar(
-                                      backgroundColor: Colors.amber,
-                                    ),
-                                  ),
-                                  Container(
-                                    padding: EdgeInsets.only(top: 10),
-                                    child: Column(
-                                      children: [
-                                        Container(
-                                          padding: EdgeInsets.only(left: 10),
-                                          child: Text(
-                                            'Bom dia!',
-                                            style: TextStyle(fontSize: 12),
-                                          ),
-                                        ),
-                                        Container(
-                                          padding: EdgeInsets.only(left: 10),
-                                          child: Text('Matheus Soldera'),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () => Get.to(UserProfilePage()),
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(top: 10),
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              color: Color.fromARGB(255, 233, 233, 233),
                             ),
-                            Container(
-                              padding: EdgeInsets.only(right: 10),
-                              child: Text('Ver meu perfil'),
+                            height: 60,
+                            width: MediaQuery.of(context).size.width - 20,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.only(left: 10),
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        child: CircleAvatar(
+                                          backgroundColor: Colors.amber,
+                                        ),
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.only(top: 10),
+                                        child: Column(
+                                          children: [
+                                            Container(
+                                              padding:
+                                                  EdgeInsets.only(left: 10),
+                                              child: Text(
+                                                'Bom dia!',
+                                                style: TextStyle(fontSize: 12),
+                                              ),
+                                            ),
+                                            Container(
+                                              padding:
+                                                  EdgeInsets.only(left: 10),
+                                              child: Text('Matheus Soldera'),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.only(right: 10),
+                                  child: Text('Ver meu perfil'),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
+                  Container(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 200,
+                            child: ListView.builder(
+                                itemCount: 10,
+                                scrollDirection: Axis.horizontal,
+                                itemBuilder: (context, index) => Container(
+                                      height: 100,
+                                      width: 100,
+                                      margin: EdgeInsets.all(10),
+                                      child: Center(
+                                        child: Text('Card $index'),
+                                      ),
+                                      color: Colors.amber,
+                                    )),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
           ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color(0xFF000959),
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        color: Color(0xFF000959),
-        child: IconTheme(
-          data: IconThemeData(color: Color.fromARGB(255, 240, 240, 240)),
-          child: Padding(
-            padding: EdgeInsets.all(12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  onPressed: () => Get.to(
-                    HomePage(),
-                    transition: Transition.noTransition,
+          Container(
+              alignment: Alignment.bottomCenter,
+              child: BottomNavigationBar(
+                selectedItemColor: Color.fromRGBO(0, 9, 89, 1),
+                unselectedItemColor: Color.fromARGB(255, 59, 59, 59),
+                backgroundColor: Color.fromARGB(255, 233, 233, 233),
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
                   ),
-                  icon: Icon(Icons.home),
-                ),
-                IconButton(
-                  onPressed: () =>
-                      Get.to(SearchPage(), transition: Transition.noTransition),
-                  icon: Icon(Icons.search),
-                ),
-                SizedBox(
-                  width: 24,
-                ),
-                IconButton(
-                  onPressed: () => Get.to(UserProfilePage(),
-                      transition: Transition.noTransition),
-                  icon: Icon(Icons.account_box_sharp),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.list),
-                )
-              ],
-            ),
-          ),
-        ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.search),
+                    label: 'Pesquisar',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.account_box_rounded),
+                    label: 'Perfil',
+                  ),
+                ],
+                currentIndex: _selectedIndex,
+                onTap: _OnSelectedItem,
+              ))
+        ],
       ),
     );
   }
---
+++ lib/Views/tutorial-page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:how_to/Views/home.dart';
+import 'package:how_to/Views/search-page.dart';
+import 'package:how_to/Views/user-profile.dart';
+import 'package:how_to/Views/user-register.dart';
+import 'package:get/get.dart';
+
+class TutorialPage extends StatefulWidget {
+  @override
+  State<TutorialPage> createState() => _TutorialPageState();
+}
+
+class _TutorialPageState extends State<TutorialPage> {
+  int _selectedIndex = 0;
+  void _OnSelectedItem(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    switch (index) {
+      case 0:
+        Get.to(HomePage());
+        break;
+      case 1:
+        Get.to(SearchPage());
+        break;
+      case 2:
+        Get.to(UserProfilePage());
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: Color(0xFF000959),
+          ),
+          Positioned(
+            top: 70,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  spreadRadius: 8,
+                  blurRadius: 10,
+                  offset: Offset(0, 2), // changes position of shadow
+                ),
+              ], color: Color.fromARGB(255, 240, 240, 240)),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    alignment: Alignment.topLeft,
+                    padding: EdgeInsets.only(left: 10, top: 10),
+                    child: GestureDetector(
+                      onTap: () => Get.back(),
+                      child: Icon(
+                        Icons.arrow_back_outlined,
+                        color: Color.fromRGBO(0, 9, 89, 1),
+                      ),
+                    ),
+                  ),
+                  Container(
+                      child: const Text(
+                    'Tutorial',
+                    style: TextStyle(fontSize: 35),
+                  ))
+                ],
+              ),
+            ),
+          ),
+          Container(
+              alignment: Alignment.bottomCenter,
+              child: BottomNavigationBar(
+                selectedItemColor: Color.fromRGBO(0, 9, 89, 1),
+                unselectedItemColor: Color.fromARGB(255, 59, 59, 59),
+                backgroundColor: Color.fromARGB(255, 233, 233, 233),
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.search),
+                    label: 'Pesquisar',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.account_box_rounded),
+                    label: 'Perfil',
+                  ),
+                ],
+                currentIndex: _selectedIndex,
+                onTap: _OnSelectedItem,
+              ))
+        ],
+      ),
+    );
+  }
+}
