ec6050445d30ff999ba4036cd03d34617e653dd0
Pedro Soldera <pedro-soldera2011@hotmail.com>
2023-05-13 15:05:09 -0300

    Adicionando First page

--- lib/Views/first_page.dart
+++ lib/Views/first_page.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:how_to/Views/createTutorial-page.dart';
 import 'package:how_to/Views/home.dart';
 import 'package:how_to/Views/search-page.dart';
 import 'package:how_to/Views/user-profile.dart';
@@ -17,9 +16,68 @@ class FirstPage extends StatefulWidget {
 
 class _FirstPageState extends State<FirstPage> {
   int paginaAtual = 0;
-  bool isAdm = true;
   late PageController pc;
   FirebaseAuth auth = FirebaseAuth.instance;
+
+  void logOut(BuildContext context) async {
+    try {
+      await auth.signOut();
+
+      Navigator.of(context).pushNamed('/');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _popUp(context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 10,
+            titlePadding: EdgeInsets.all(5),
+            title: Text('Sair'),
+            backgroundColor: Color.fromARGB(255, 240, 240, 240),
+            content: Text('Você realmente deseja sair do aplicativo?'),
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: () => Get.back(),
+                    child: Container(
+                      padding: EdgeInsets.only(top: 5),
+                      height: 30,
+                      width: 80,
+                      child: Text(
+                        'Não',
+                        style: TextStyle(color: Color.fromRGBO(0, 9, 89, 1)),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => logOut(context),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(0, 9, 89, 1),
+                          borderRadius: BorderRadius.circular(5)),
+                      padding: EdgeInsets.only(top: 5),
+                      height: 30,
+                      width: 80,
+                      child: Text(
+                        'Sim',
+                        style: TextStyle(color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          );
+        });
+  }
 
   @override
   void initState() {
@@ -42,7 +100,6 @@ class _FirstPageState extends State<FirstPage> {
         children: [
           HomePage(),
           SearchPage(),
-          CreateTutorialPage(),
           UserProfilePage(),
         ],
         onPageChanged: setPaginaAtual,
@@ -62,31 +119,21 @@ class _FirstPageState extends State<FirstPage> {
             label: 'Pesquisar',
             backgroundColor: Color.fromARGB(255, 0, 9, 89),
           ),
-          if (isAdm)
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add),
-              label: 'Novo Tutorial',
-              backgroundColor: Color.fromARGB(255, 0, 9, 89),
-            ),
           BottomNavigationBarItem(
             icon: Icon(Icons.account_circle_outlined),
             label: 'Perfil',
             backgroundColor: Color.fromARGB(255, 0, 9, 89),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.logout),
+            icon: GestureDetector(
+              child: Icon(Icons.logout),
+              onTap: () => _popUp(context),
+            ),
             label: 'Sair',
             backgroundColor: Color.fromARGB(255, 0, 9, 89),
-          ),
+          )
         ],
         onTap: (pagina) {
-          if (pagina != 3) {
-            setState(() {
-              paginaAtual = pagina;
-            });
-          } else {
-            Get.to(CreateTutorialPage());
-          }
           pc.animateToPage(pagina,
               duration: Duration(milliseconds: 450), curve: Curves.ease);
         },
