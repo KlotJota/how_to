9f0b67e1ef26abc86b8846f1eb47f1eab8e00ac8
Pedro Soldera <pedro-soldera2011@hotmail.com>
2023-05-14 23:19:53 -0300

    implementando funções na pagina do tutorial

--- lib/Views/tutorial-page.dart
+++ lib/Views/tutorial-page.dart
@@ -21,116 +21,120 @@ class TutorialPage extends StatefulWidget {
 class _TutorialPageState extends State<TutorialPage> {
   FirebaseAuth auth = FirebaseAuth.instance;
 
-  // final DocumentSnapshot tutorial = FirebaseFirestore.instance
-  //     .collection('tutoriais')
-  //     .doc('id') as DocumentSnapshot<Object?>;
+  DocumentSnapshot<Object?>? tutorial;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTutorial();
+  }
+
+  void fetchTutorial() async {
+    final tutorialDoc = await FirebaseFirestore.instance
+        .collection('tutoriais')
+        .doc(widget.id)
+        .get();
+    setState(() {
+      tutorial = tutorialDoc;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            alignment: Alignment.topCenter,
-            child: Container(
-              margin: EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      scale: 19,
-                      alignment: Alignment.topCenter,
-                      image: AssetImage('images/how-to-branco.png'))),
-            ),
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(0, 9, 89, 1),
-            ),
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-          ),
-          Positioned(
-            top: 70,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              decoration: BoxDecoration(boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  spreadRadius: 8,
-                  blurRadius: 10,
-                  offset: Offset(0, 2), // changes position of shadow
+        body: tutorial == null
+            ? CircularProgressIndicator()
+            : Stack(children: [
+                Container(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    margin: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            scale: 19,
+                            alignment: Alignment.topCenter,
+                            image: AssetImage('images/how-to-branco.png'))),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(0, 9, 89, 1),
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
                 ),
-              ], color: Color.fromARGB(255, 240, 240, 240)),
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(bottom: 10),
-                    alignment: Alignment.topLeft,
-                    padding: EdgeInsets.only(left: 10, top: 10),
-                    child: GestureDetector(
-                      onTap: () => Get.back(),
-                      child: Icon(
-                        Icons.arrow_back_outlined,
-                        color: Color.fromRGBO(0, 9, 89, 1),
-                      ),
-                    ),
-                  ),
-                  Container(
-                      margin: EdgeInsets.all(10),
-                      alignment: Alignment.topCenter,
-                      child: Text(
-                        '',
-                        style: TextStyle(fontSize: 30),
-                      )),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    height: 200,
-                    child: Column(
-                      children: [
-                        Expanded(
-                            child: Stack(
-                          fit: StackFit.expand,
-                          children: [
-                            FittedBox(
-                                fit: BoxFit.fill,
-                                child: Image.asset('images/lucas.jpeg')),
-                          ],
-                        )),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: Text(
-                        'Você provavelmente durante sua vida escolar já precisou cuidar de sua documentação afinal, são tantas que você as vezes até acabar perdido. Por isso, este tutorial irá lhe ensinar desde como escolher a foto perfeita para seus documentos, quanto quais dados levar na hora de fazê-los. '),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: Text(
-                        'Você provavelmente durante sua vida escolar já precisou cuidar de sua documentação afinal, são tantas que você as vezes até acabar perdido. Por isso, este tutorial irá lhe ensinar desde como escolher a foto perfeita para seus documentos, quanto quais dados levar na hora de fazê-los. '),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: Text(
-                        'Você provavelmente durante sua vida escolar já precisou cuidar de sua documentação afinal, são tantas que você as vezes até acabar perdido. Por isso, este tutorial irá lhe ensinar desde como escolher a foto perfeita para seus documentos, quanto quais dados levar na hora de fazê-los. '),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: Text(
-                        'Você provavelmente durante sua vida escolar já precisou cuidar de sua documentação afinal, são tantas que você as vezes até acabar perdido. Por isso, este tutorial irá lhe ensinar desde como escolher a foto perfeita para seus documentos, quanto quais dados levar na hora de fazê-los. '),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: Text(
-                        'Você provavelmente durante sua vida escolar já precisou cuidar de sua documentação afinal, são tantas que você as vezes até acabar perdido. Por isso, este tutorial irá lhe ensinar desde como escolher a foto perfeita para seus documentos, quanto quais dados levar na hora de fazê-los. '),
-                  )
-                ],
-              ),
-            ),
-          ),
-          Container(
-            height: 150,
-          )
-        ],
-      ),
-    );
+                Positioned(
+                    top: 70,
+                    child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height,
+                        decoration: BoxDecoration(boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            spreadRadius: 8,
+                            blurRadius: 10,
+                            offset: Offset(0, 2), // changes position of shadow
+                          ),
+                        ], color: Color.fromARGB(255, 240, 240, 240)),
+                        child: ListView(children: [
+                          Container(
+                            margin: EdgeInsets.only(bottom: 10),
+                            alignment: Alignment.topLeft,
+                            padding: EdgeInsets.only(left: 10, top: 10),
+                            child: GestureDetector(
+                              onTap: () => Get.back(),
+                              child: Icon(
+                                Icons.arrow_back_outlined,
+                                color: Color.fromRGBO(0, 9, 89, 1),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            alignment: Alignment.topCenter,
+                            child: Text(
+                              tutorial!['titulo'],
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 200,
+                            child: Column(
+                              children: [
+                                Expanded(
+                                    child: Stack(
+                                  fit: StackFit.expand,
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              color: Colors.black, width: 2.0),
+                                          borderRadius:
+                                              BorderRadius.circular(10.0)),
+                                      child: Image.network(
+                                        tutorial!['imagem'],
+                                        fit: BoxFit.cover,
+                                        alignment: Alignment.center,
+                                      ),
+                                    )
+                                  ],
+                                )),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 200,
+                            child: Container(
+                              child: Text(tutorial!['texto'],
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  )),
+                            ),
+                          )
+                        ])))
+              ]));
   }
 }
