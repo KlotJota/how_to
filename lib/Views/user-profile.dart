a3179cb250d55483dd84090cbcc104258505676c
Jonathan Speçamillio <jgspecamillio@gmail.com>
2023-06-01 21:28:24 -0300

    Finalização da tela de perfil, adição, remoção e clique nos favoritos

--- lib/Views/user-profile.dart
+++ lib/Views/user-profile.dart
@@ -134,7 +134,119 @@ class _UserProfilePageState extends State<UserProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: favoritos.isEmpty
-            ? null
+            ? Stack(
+                children: [
+                  Container(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              scale: 19,
+                              alignment: Alignment.topCenter,
+                              image: AssetImage('images/how-to-branco.png'))),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(0, 9, 89, 1),
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                  ),
+                  Positioned(
+                    top: 70,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                      decoration: BoxDecoration(boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: 8,
+                          blurRadius: 10,
+                          offset: Offset(0, 2), // changes position of shadow
+                        ),
+                      ], color: Color.fromARGB(255, 243, 243, 243)),
+                      child: ListView(
+                        physics: BouncingScrollPhysics(),
+                        children: [
+                          Card(
+                            shadowColor: Colors.black,
+                            margin: EdgeInsets.fromLTRB(15, 10, 15, 0),
+                            color: Color.fromARGB(255, 250, 247, 247),
+                            elevation: 5,
+                            child: Column(
+                              children: [
+                                Container(
+                                    margin: EdgeInsets.only(top: 40),
+                                    child: auth.currentUser!.photoURL == null
+                                        ? CircleAvatar(
+                                            backgroundImage: NetworkImage(
+                                                'https://firebasestorage.googleapis.com/v0/b/howto-60459.appspot.com/o/perfis%2Fpadr%C3%A3o%2Fuser.png?alt=media&token=bb4a0f5c-8839-400d-8fb3-dbaaf07b3117'),
+                                            radius: 40,
+                                          )
+                                        : GestureDetector(
+                                            onTap: () {
+                                              popUpImagem();
+                                            },
+                                            child: CircleAvatar(
+                                              backgroundImage: NetworkImage(auth
+                                                  .currentUser!.photoURL
+                                                  .toString()),
+                                              radius: 40,
+                                            ),
+                                          )),
+                                Container(
+                                    alignment: Alignment.center,
+                                    margin: EdgeInsets.only(top: 10),
+                                    child: auth.currentUser!.displayName == null
+                                        ? Text(
+                                            'Usuário',
+                                            style: TextStyle(fontSize: 18),
+                                          )
+                                        : Text(
+                                            auth.currentUser!.displayName
+                                                .toString(),
+                                            style: TextStyle(fontSize: 18),
+                                          )),
+                                Container(
+                                  margin: EdgeInsets.only(top: 40),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        margin: EdgeInsets.only(top: 10),
+                                        child: Column(
+                                          children: [
+                                            Container(
+                                                margin:
+                                                    EdgeInsets.only(bottom: 10),
+                                                child: Text(
+                                                  'Tutoriais Salvos',
+                                                  style:
+                                                      TextStyle(fontSize: 25),
+                                                )),
+                                            Container(
+                                                margin:
+                                                    EdgeInsets.only(bottom: 10),
+                                                child: Text(
+                                                  favoritos.length.toString(),
+                                                  style:
+                                                      TextStyle(fontSize: 23),
+                                                ))
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
             : StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
                 stream: firestore
                     .collection('tutoriais')
